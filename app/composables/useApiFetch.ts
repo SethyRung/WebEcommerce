790776f8cc786a284ca776b5aa01406cd61511f1
Sethy Rung <rungsethyhk@gmail.com>
2026-04-14 18:02:34 +0700

    refactor: set sever to false and remove cookie forwarding logic and simplify response handling in useApiFetch

--- app/composables/useApiFetch.ts
+++ app/composables/useApiFetch.ts
@@ -1,24 +1,13 @@
 export const useApiFetch = createUseFetch((options) => {
   const config = useRuntimeConfig();
   const REFRESH_TOKEN_ENDPOINT = "/api/auth/refresh";
-  const event = import.meta.server ? useRequestEvent() : null;
-
-  function forwardCookiesToClient(setCookieHeaders: string[]) {
-    if (event) {
-      for (const cookie of setCookieHeaders) {
-        appendHeader(event, "set-cookie", cookie);
-      }
-    }
-  }
 
   return {
     baseURL: config.public.baseURL,
     timeout: 10000,
+    server: false,
     onResponse: async (context) => {
       if (context.response.ok) {
-        const cookies = context.response.headers.getSetCookie();
-        forwardCookiesToClient(cookies);
-
         const data = context.response._data as ApiResponse<unknown>;
         if (data.status.code === ApiResponseCode.Unauthorized) {
           const refreshTokenRes = await $fetch.raw(REFRESH_TOKEN_ENDPOINT, {
@@ -38,21 +27,17 @@ export const useApiFetch = createUseFetch((options) => {
 
           const refreshCookies = refreshTokenRes.headers.getSetCookie();
 
-          const cookies = refreshCookies.join("; ");
+          const cookie = refreshCookies.join("; ");
           const repeatOptions = {
             ...context.options,
             onResponse: undefined,
             headers: {
               ...context.options.headers,
-              cookie: cookies,
+              cookie,
             },
           };
 
           const repeatRes = await $fetch.raw(context.request, repeatOptions as any);
-          const repeatCookies = repeatRes.headers.getSetCookie();
-
-          forwardCookiesToClient([...refreshCookies, ...repeatCookies]);
-
           context.response._data = repeatRes._data;
         }
       }
